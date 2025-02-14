2d0855c78b825ab1783bef11500e3bec217c0b74
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-02-15 01:07:07 +0530

    learn comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+/*
+    console.log(2 > 1);
+    console.log(2 >= 1);
+    console.log(2 < 1);
+    console.log(2 == 1);
+    console.log(2 != 1);
+*/
+/*
+    // confusing comparisons
+    console.log("2" > 1);
+    console.log("02" > 1);
+    //when these types of conversions takes place then sometime the comparisons does not give predictable result;
+    // so Whenever we are doing comparisons , make sure the both have the same data type
+
+    //examples of getting unpredictable comparison result
+    console.log(null > 0);
+    console.log(null == 0);
+    console.log(null >= 0);
+
+    //The reason is that  an equality check == and comparisons > ,<, >=, <= work differently
+    //comparisons convert nulll to a number ,treating it as 0
+    //That's why (3) null >= 0 is true and (1) null > 0 is false
+*/
+
+
+//Strict Check (===)
+//definition : It not only checks your values but it also checks its data types
+//example
+console.log("2" === 2);
