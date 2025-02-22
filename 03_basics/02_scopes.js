9c5dc6f0324ff9ed791dd3a6a8dc8bf114be7ec8
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-02-22 19:26:13 +0530

    learn scopes in javascript

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,35 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//problem
+    // var c = 300;
+    // if (true){ // the values written inside this is called local scope and outside it is global scope
+    //     let a = 10;
+    //     const b = 20
+    //     var c = 30;
+    // }
+    // // console.log(a);
+    // // console.log(b);
+    // console.log(c);// ye c print nhi honi chahiye thi  c bahar nhi aana chahiye tha
+
+// let ki kahani 
+    // let a = 300;
+    // if(true){
+    //     let a = 10;
+    //     const b = 20
+    //     console.log("INNER :" ,a);
+    // }
+    // console.log(a);
+
+
+// scopes in windows
+//Interview Question : the scopes in browser console option and the scope in code environment if we run example through node environment then scope will be different
+    
+
+
+
