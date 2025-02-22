602eeeaaf157073fbb8669fca3c91fc7fda10a42
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-02-22 20:42:31 +0530

    mini hoisting in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -31,5 +31,58 @@
 //Interview Question : the scopes in browser console option and the scope in code environment if we run example through node environment then scope will be different
     
 
+//********************************Scope level and Mini Hoisting in javascript************ */
+// function one(){
+//     const username = "Safari"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+
+//     }
+//     // console.log(website);
+//     two()
+// }
+
+// one()
+
+
+// the above works also happens inif else
+
+if(true){
+    const username = "Altroz"
+    if(username === "Altroz") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website) this will generate error so commented
+}
+// console.log(username); // this will also generate error so commented
+
+
+
+//***********+++++++++++++++Interesting Concept *****************++++++++++++ */
+
+// function addone(num){ // this is basic function 
+//     return num + 1;
+// }
+// addone(5);
+
+// const addTwo = function(num){ // isko kabhi kabhi expression bhi bol diya jaata hai , this is like variables in javscript variables is very powerful it can hold anything , can hold JSON, functions etc
+//     return num + 2;
+// }
+// addTwo(5);
+
+// now comes the intersting part 
+//the below line codes runs fines 
+addone(5); //function calling this way is accepted
+function addone(num){ // this is basic function 
+    return num + 1;
+}
+addTwo(5); // this will generate error because  addTwo is unaccessible ,this problem is arising beacuse we hold that function along with declaration ,,,this is better understand at hoisting concept
+const addTwo = function(num){ // isko kabhi kabhi expression bhi bol diya jaata hai , this is like variables in javscript variables is very powerful it can hold anything , can hold JSON, functions etc
+    return num + 2;
+}
+
 
 
