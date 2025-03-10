8b91a50f2aadfedbb1194d716643ab1f70ad3620
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-10 16:56:43 +0530

    events and async basics intro

---
+++ 09_Async/01_notes.js
@@ -0,0 +1,30 @@
+/*
+
+** Javascript is a synchronous language, Synchronous langauage means the code will execute line by line
+   Javascript is a single threaded language 
+
+
+**** Execution Context
+        execute line one by one
+
+
+    Blocking code vs Non Blocking Code
+
+    Blocking Code:- Block the flow of program
+        ->Read File Sync
+
+
+
+    Non-Blocking Code:- Does not block execution
+        ->Read File Async
+
+
+
+
+        fetch() :-> this also work on task queue, it expands the task queue,we can also say ek dusri task queue laga diya gya jo ki high priority ki hai
+                //In fetch we have a concept of promise which says ye kaam thori der baad kar dena agar hua to bata dena aur nhi hua to bhi bata dena, aise kaam ke results 
+                  fetch ke through aate hai
+
+
+
+    */
