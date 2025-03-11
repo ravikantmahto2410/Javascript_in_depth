d93c316d920916df7d896bc91b39c96a31ecd115
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-11 16:48:49 +0530

    Print Name and Date after some interval

--- 07_projects/05_UnlimitedColors/index.js
+++ 07_projects/05_UnlimitedColors/index.js
@@ -33,7 +33,7 @@
 const randomColor = function(){
     const hex = '0123456789ABCDEF';
     let color = '#';
-    for(let i = 0; i < 6; i ++){
+    for(let i = 0; i < 6; i++){
       color += hex[Math.floor(Math.random() * 10)];
   
     }
