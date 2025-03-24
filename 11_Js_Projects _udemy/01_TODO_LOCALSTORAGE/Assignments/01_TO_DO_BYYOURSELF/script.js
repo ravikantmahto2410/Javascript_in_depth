7cb7a5af072c81bfed7fea6d616d4c794aa3328b
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-24 07:45:00 +0530

    make a to do list

--- 11_Js_Projects _udemy/01_TODO_LOCALSTORAGE/Assignments/01_TO_DO_BYYOURSELF/script.js
+++ 11_Js_Projects _udemy/01_TODO_LOCALSTORAGE/Assignments/01_TO_DO_BYYOURSELF/script.js
@@ -1,22 +1,55 @@
-const todoInput = document.getElementById('todo-input')
-const addTask = document.getElementById('add-task-btn')
-const todoList = document.getElementById('todo-list')
+document.addEventListener('DOMContentLoaded',() => {
+    const todoInput = document.getElementById('todo-input')
+    const addTask = document.getElementById('add-task-btn')
+    const todoList = document.getElementById('todo-list')
 
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || []
 
-let tasks = []
+    tasks.forEach(task => renderTasks(task))
 
-addTask.addEventListener('click', () => {
-    const taskText = todoInput.value.trim()
-    if(todoInput === '') return
+    addTask.addEventListener('click', () => {
+        const taskText = todoInput.value.trim()
+        if(todoInput === '') return
 
-    document.createElement(li)
-    
-    const newTask = {
-        id : Date.now(),
-        text : taskText,
-        isCompleted : false
+        const newTask = {
+            id : Date.now(),
+            text : taskText,
+            isCompleted : false
+        }
+        tasks.push(newTask)
+        saveTasks();
+        renderTasks(newTask)
+        todoInput.value = '' // to clear the input
+        console.log(tasks)
+
+    })
+
+    function renderTasks(task){ //this will pickup the tasks form the local storage and we will grab it
+        const li = document.createElement('li')
+        li.setAttribute('data-id', task.id)
+        if(task.completed) li.classList.add('completed')
+
+        li.innerHTML = `
+        <span>${task.text}</span>
+        <button>Delete</button>
+        `
+        li.addEventListener('click',(e)=>{
+            if(e.target.tagName === 'BUTTON') return
+            task.completed = !task.completed
+            li.classList.toggle('completed')
+            saveTasks()
+        })
+        li.querySelector('button').addEventListener('click', (e) => {
+            e.stopPropagation() //this is to prevent toggle from firing
+            tasks = tasks.filter(t => t.id !== task.id)
+            li.remove()
+            saveTasks()
+        })
+
+        todoList.appendChild(li)
     }
-
-    todoInput.value = '' // to clear the input
+    function saveTasks(){ // this function is used to add tasks to the local storage
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
 
 })
